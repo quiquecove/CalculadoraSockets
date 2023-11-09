a5b14b7a9298bf1d61da7ea0486f8758601e744b
Enrique Huerta Lopez <quiquehuertalopez@gmail.com>
2023-11-09 20:39:46 +0100

    calculadora hecha

---
+++ src/ActividadSockets/Cliente/ClienteSocket.java
@@ -0,0 +1,111 @@
+package ActividadSockets.Cliente;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class ClienteSocket {
+    public static final int PUERTO = 2017;
+    public static final String IP_SERVER = "localhost";
+
+    public static void main(String[] args) throws IOException {
+        String numero1;
+        String numero2;
+        String operandos = null;
+        PrintStream salida;
+        System.out.println("        APLICACI�N CLIENTE         ");
+        System.out.println("-----------------------------------");
+
+        InetSocketAddress direccionServidor = new InetSocketAddress(IP_SERVER, PUERTO);
+
+
+
+
+            try (Scanner sc = new Scanner(System.in);
+                 Socket socketAlServidor = new Socket()) {
+                while (true) {
+                    System.out.println("Que operacion desea realizar?");
+                    System.out.println("1-Sumar\n2-Restar\n3-Multiplicar\n4-Dividir");
+                    int eleccion = sc.nextInt();
+                    sc.nextLine(); // Consumir el carácter de nueva línea
+
+                    switch (eleccion) {
+                        case 1:
+                            System.out.println("CLIENTE: Introduzca los numeros a sumar");
+                            numero1 = sc.nextLine();
+                            numero2 = sc.nextLine();
+                            operandos = numero1 + "_" + numero2;
+                            socketAlServidor.connect(direccionServidor);
+                            salida = new PrintStream(socketAlServidor.getOutputStream());
+                            salida.println(operandos);
+                            break;
+                        case 2:
+                            System.out.println("CLIENTE: Introduzca los numeros a restar");
+                            numero1 = sc.nextLine();
+                            numero2 = sc.nextLine();
+                            operandos = numero1 + "-" + numero2;
+                            socketAlServidor.connect(direccionServidor);
+                            salida = new PrintStream(socketAlServidor.getOutputStream());
+                            salida.println(operandos);
+                            break;
+                        case 3:
+                            System.out.println("CLIENTE: Introduzca los numeros a multiplicar");
+                            numero1 = sc.nextLine();
+                            numero2 = sc.nextLine();
+                            operandos = numero1 + "m" + numero2;
+                            socketAlServidor.connect(direccionServidor);
+                            salida = new PrintStream(socketAlServidor.getOutputStream());
+                            salida.println(operandos);
+                            break;
+                        case 4:
+                            System.out.println("CLIENTE: Introduzca los numeros a dividir");
+                            numero1 = sc.nextLine();
+                            numero2 = sc.nextLine();
+                            operandos = numero1 + "/" + numero2;
+                            socketAlServidor.connect(direccionServidor);
+                            salida = new PrintStream(socketAlServidor.getOutputStream());
+                            salida.println(operandos);
+                            break;
+                        default:
+                            System.out.println("Error, operacion no valida");
+                    }
+
+
+                    System.out.println("CLIENTE: Esperando a que el servidor acepte la conexi�n");
+                    //socketAlServidor.connect(direccionServidor);
+                    System.out.println("CLIENTE: Conexion establecida... a " + IP_SERVER
+                            + " por el puerto " + PUERTO);
+
+                    //Creamos el objeto que nos permite mandar informacion al servidor
+                    //PrintStream salida = new PrintStream(socketAlServidor.getOutputStream());
+                    //Mandamos la informaci�n por el Stream
+
+                    InputStreamReader entrada = new InputStreamReader(socketAlServidor.getInputStream());
+
+                    BufferedReader bf = new BufferedReader(entrada);
+
+                    System.out.println("CLIENTE: Esperando al resultado del servidor...");
+                    String resultado = bf.readLine();//7
+
+                    System.out.println("CLIENTE: El resultado de la operacion es: " + resultado);//7
+                }
+            } catch (UnknownHostException e) {
+                System.err.println("CLIENTE: No encuentro el servidor en la dirección" + IP_SERVER);
+                e.printStackTrace();
+            } catch (IOException e) {
+                System.err.println("CLIENTE: Error de entrada/salida");
+                e.printStackTrace();
+            } catch (Exception e) {
+                System.err.println("CLIENTE: Error -> " + e);
+                e.printStackTrace();
+            }
+
+            System.out.println("CLIENTE: Fin del programa");
+        }
+
+}
---
+++ src/ActividadSockets/Servidor/ServidorSocket.java
@@ -0,0 +1,123 @@
+package ActividadSockets.Servidor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServidorSocket {
+        public static final int PUERTO = 2017;
+
+        public static void main(String[] args) throws InterruptedException {
+            System.out.println("      APLICACI�N DE SERVIDOR      ");
+            System.out.println("----------------------------------");
+
+            //ServidorSocket -> Este objeto es el que estar� escuchando peticiones por un puerto
+            //y creara un objeto socket por cada peticion
+
+            //Entrada de datos. Es el canal de entrada del servidor, es decir, el canal por
+            //el cual el cliente nos va a mandar la informaci�n.
+            InputStreamReader entrada = null;
+            //Salida de datos. Es el canal de salida del servidor, es decir, el canal por
+            //el cual vamos a enviar informaci�n al cliente.
+            PrintStream salida = null;
+
+            //Notese como ahora:
+            //1. El PrintStream del cliente es el InputStreamReader del servidor
+            //2. El PrintStream del servidor es el InputStreamReader del cliente
+
+            //Socket -> es la clase que nos va a permitir comunicarnos con el cliente,
+            //en este caso no lo crearemos nosotros, sino que sera el SocketServer quien
+            //lo cree cuando acepte una peticion de un cliente.
+            Socket socketAlCliente = null;
+
+            InetSocketAddress direccion = new InetSocketAddress(PUERTO);
+
+            //En este caso no podemos hacer la declaracion try-with-resources como antes
+            //ya que el servidor es un hilo que no para nunca y por cada peticion
+            //crea un nuevo objeto Socket a partir del objeto ServerSocket, es decir,
+            //sera el objeto ServerSocket el que nos crerara el objeto Socket por nosostros
+            try (ServerSocket serverSocket = new ServerSocket()){
+
+                //Decimos al server socket que escuche peticiones desde el puerto
+                //que hayamos establecido
+                serverSocket.bind(direccion);
+
+                //Vamos a llevar la cuenta del numero de peticiones que nos llegan
+                int peticion = 0;
+
+                //Estamos continuamente escuchando, es lo normal dentro del comportamiento
+                //de un servidor, un programa que no para nunca
+                while(true){
+
+                    System.out.println("SERVIDOR: Esperando peticion por el puerto " + PUERTO);
+
+                    //En este punto, se parara el programa, hasta que entre la peticion de
+                    //un cliente, y sera en ese momento cuando se cree un objeto Socket
+                    socketAlCliente = serverSocket.accept();
+                    System.out.println("SERVIDOR: peticion numero " + ++peticion + " recibida");
+
+                    entrada = new InputStreamReader(socketAlCliente.getInputStream());
+                    BufferedReader bf = new BufferedReader(entrada);
+
+                    //El servidor se quedará aquí parado hasta que el cliente nos mande
+                    //informacion, es decir, cuando haga un salida.println(INFORMACION);
+                    String stringRecibido = bf.readLine();//3-4
+
+                    //Hay que tener en cuenta que toda comunicacion entre cliente y servidor
+                    //esta en formato de cadena de texto
+                    System.out.println("SERVIDOR: Me ha llegado del cliente: " + stringRecibido);
+                    //Como sabemos que el cliente nos envia un 3-7, hacemos un split por "-"
+                    //para obtener la información.
+                    if(stringRecibido.contains("_")) {//suma
+                        String[] operadores = stringRecibido.split("_");
+                        int iNumero1 = Integer.parseInt(operadores[0]);//3
+                        int iNumero2 = Integer.parseInt(operadores[1]);//4
+                        int resultado = iNumero1 + iNumero2;//7
+                        System.out.println("SERVIDOR: La suma de los numeros es: " + resultado);
+                        salida = new PrintStream(socketAlCliente.getOutputStream());
+                        salida.println(resultado);
+                    } else if (stringRecibido.contains("-")) {//resta
+                        String[] operadores = stringRecibido.split("-");
+                        int iNumero1 = Integer.parseInt(operadores[0]);//3
+                        int iNumero2 = Integer.parseInt(operadores[1]);//4
+                        int resultado = iNumero1 - iNumero2;//7
+                        System.out.println("SERVIDOR: La resta de los numeros es: " + resultado);
+                        salida = new PrintStream(socketAlCliente.getOutputStream());
+                        salida.println(resultado);
+
+                    } else if (stringRecibido.contains("m")) {//multiplicacion
+                        String[] operadores = stringRecibido.split("m");
+                        int iNumero1 = Integer.parseInt(operadores[0]);//3
+                        int iNumero2 = Integer.parseInt(operadores[1]);//4
+                        int resultado = iNumero1 * iNumero2;//7
+                        System.out.println("SERVIDOR: La multiplicacion de los numeros es: " + resultado);
+                        salida = new PrintStream(socketAlCliente.getOutputStream());
+                        salida.println(resultado);
+                    } else if (stringRecibido.contains("/")) {//division
+                        String[] operadores = stringRecibido.split("/");
+                        int iNumero1 = Integer.parseInt(operadores[0]);//3
+                        int iNumero2 = Integer.parseInt(operadores[1]);//4
+                        int resultado = iNumero1 / iNumero2;//7
+                        System.out.println("SERVIDOR: La division de los numeros es: " + resultado);
+                        salida = new PrintStream(socketAlCliente.getOutputStream());
+                        salida.println(resultado);
+                    }
+                    //Si hemos llegado hasta aqui, cerramos las conexiones
+                    socketAlCliente.close();
+
+                }
+            } catch (IOException e) {
+                System.err.println("SERVIDOR: Error de entrada/salida");
+                e.printStackTrace();
+            } catch (Exception e) {
+                System.err.println("SERVIDOR: Error -> " + e);
+                e.printStackTrace();
+            }
+        }//FIN DEL PROGRAMA
+
+
+}
